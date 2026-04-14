680a2c781763acd6a70954724187a7b40d8a7718
tranqn <namquoctran24@gmail.com>
2026-04-14 08:57:00 +0000

    refactor(tutorial03): update index view to use render() with template

--- module_05_django_basics/tutorial_03_views_templates/mysite/polls/views.py
+++ module_05_django_basics/tutorial_03_views_templates/mysite/polls/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
