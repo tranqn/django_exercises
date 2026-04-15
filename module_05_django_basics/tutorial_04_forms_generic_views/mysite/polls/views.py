3bf48ea30d228a5d7d35dcfbd3069ec62aeb2118
tranqn <namquoctran24@gmail.com>
2026-04-15 09:20:00 +0000

    refactor(tutorial04): convert index view to IndexView using ListView

--- module_05_django_basics/tutorial_04_forms_generic_views/mysite/polls/views.py
+++ module_05_django_basics/tutorial_04_forms_generic_views/mysite/polls/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 
 def detail(request, question_id):
