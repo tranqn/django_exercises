07254e689ebb79edc3f8e923c20a6f2db4d90299
tranqn <namquoctran24@gmail.com>
2026-04-15 10:12:18 +0200

    refactor(tutorial03): update detail and results views with get_object_or_404

--- module_05_django_basics/tutorial_03_views_templates/mysite/polls/views.py
+++ module_05_django_basics/tutorial_03_views_templates/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
@@ -10,11 +10,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
