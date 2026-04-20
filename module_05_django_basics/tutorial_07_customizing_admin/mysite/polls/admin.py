3db7fabacffa79b49d9de014c615089f36c543c7
tranqn <namquoctran24@gmail.com>
2026-04-20 11:11:23 +0200

    feat(tutorial07): add fieldsets to organize Question admin form

--- module_05_django_basics/tutorial_07_customizing_admin/mysite/polls/admin.py
+++ module_05_django_basics/tutorial_07_customizing_admin/mysite/polls/admin.py
@@ -7,8 +7,10 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
     inlines = [ChoiceInline]
-
-
-admin.site.register(Question, QuestionAdmin)
