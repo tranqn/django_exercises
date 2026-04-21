f505d948ad3796c5717b47b03a2ecf6e985db588
tranqn <namquoctran24@gmail.com>
2026-04-21 10:52:32 +0200

    chore: add dev settings with debug toolbar and SQLite

---
+++ module_05_django_basics/tutorial_08_third_party_packages/mysite/mysite/settings/dev.py
@@ -0,0 +1,16 @@
+"""Development-specific settings."""
+from .base import *  # noqa: F401,F403
+
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+INTERNAL_IPS = ["127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
