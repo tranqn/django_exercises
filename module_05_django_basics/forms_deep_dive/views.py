423dcb7ac311dbd809712cd93c842bcf4a4eaa68
tranqn <namquoctran24@gmail.com>
2026-04-24 07:27:00 +0000

    feat(forms): add create and update views using ModelForm

--- module_05_django_basics/forms_deep_dive/views.py
+++ module_05_django_basics/forms_deep_dive/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import ContactForm
+from .model_forms import BookForm
+from .models import Book
 
 
 def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Process form data
             name = form.cleaned_data["name"]
             messages.success(request, f"Thanks {name}, message sent!")
             return redirect("contact")
     else:
         form = ContactForm()
     return render(request, "forms_demo/contact.html", {"form": form})
+
+
+def book_create(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save()
+            messages.success(request, f"Book '{book.title}' created!")
+            return redirect("book-list")
+    else:
+        form = BookForm()
+    return render(request, "forms_demo/book_form.html", {"form": form, "action": "Create"})
+
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Book '{book.title}' updated!")
+            return redirect("book-list")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "forms_demo/book_form.html", {"form": form, "action": "Update"})
