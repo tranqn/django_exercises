0a2013af1459d966bd1c66b3f7895a1fcafec3ed
tranqn <namquoctran24@gmail.com>
2026-05-04 09:24:00 +0000

    feat(auth): add profile view with @login_required decorator

--- module_05_django_basics/auth_system/accounts/views.py
+++ module_05_django_basics/auth_system/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 def login_view(request):
@@ -36,8 +37,29 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, f"Account created! Welcome, {user.username}!")
+            messages.success(request, "Account created!")
             return redirect("home")
     else:
         form = UserRegistrationForm()
     return render(request, "accounts/register.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profile updated!")
+            return redirect("profile")
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    return render(request, "accounts/profile.html", {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    })
