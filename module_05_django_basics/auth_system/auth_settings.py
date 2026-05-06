26e129bd65ee49400958b8695ba88ebab1a4e250
tranqn <namquoctran24@gmail.com>
2026-05-06 07:47:00 +0000

    feat(auth): add LOGIN_URL and LOGIN_REDIRECT_URL settings

---
+++ module_05_django_basics/auth_system/auth_settings.py
@@ -0,0 +1,17 @@
+"""
+Authentication settings to add to settings.py:
+"""
+
+# Where to redirect after login/logout
+# LOGIN_URL = '/accounts/login/'           # Default redirect for @login_required
+# LOGIN_REDIRECT_URL = '/dashboard/'       # After successful login
+# LOGOUT_REDIRECT_URL = '/accounts/login/' # After logout
+
+# Password validation
+# AUTH_PASSWORD_VALIDATORS = [
+#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#      'OPTIONS': {'min_length': 8}},
+#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+# ]
