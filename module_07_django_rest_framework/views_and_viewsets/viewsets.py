ea5e3ea5baab962d1c1f752cd052229b288f3dd1
tranqn <namquoctran24@gmail.com>
2026-05-20 09:48:00 +0000

    feat(drf): add SearchFilter and OrderingFilter to ViewSet

--- module_07_django_rest_framework/views_and_viewsets/viewsets.py
+++ module_07_django_rest_framework/views_and_viewsets/viewsets.py
@@ -1,13 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Market, Seller
 from .serializers import MarketModelSerializer, SellerSerializer
+from .pagination import StandardPagination
 
 
 class MarketViewSet(viewsets.ModelViewSet):
     queryset = Market.objects.select_related("seller").all()
     serializer_class = MarketModelSerializer
+    pagination_class = StandardPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["name", "location", "description"]
+    ordering_fields = ["name", "created_at"]
+    ordering = ["-created_at"]
 
     @action(detail=True, methods=["get"])
     def seller_info(self, request, pk=None):
@@ -25,3 +31,5 @@ class MarketViewSet(viewsets.ModelViewSet):
 class SellerViewSet(viewsets.ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name", "city"]
